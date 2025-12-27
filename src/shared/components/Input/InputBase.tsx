33fdfc1dd32e96bac7ab0f759ccb6982ff21e80b
jeongyou <wjddbwjd1205@naver.com>
2025-12-28 05:28:58 +0900

    feat: RHF 연동을 위한 ref 추가

--- src/shared/components/Input/InputBase.tsx
+++ src/shared/components/Input/InputBase.tsx
@@ -1,12 +1,14 @@
 'use client';
 
-import { ComponentProps } from 'react';
+import { ComponentProps, forwardRef } from 'react';
 import * as S from './InputBase.styles';
 
 type InputBaseProps = ComponentProps<'input'>;
 
-const InputBase = (props: InputBaseProps) => {
-  return <S.InputBase {...props} />;
-};
+const InputBase = forwardRef<HTMLInputElement, InputBaseProps>((props, ref) => {
+  return <S.InputBase {...props} ref={ref} />;
+});
+
+InputBase.displayName = 'InputBase';
 
 export default InputBase;
