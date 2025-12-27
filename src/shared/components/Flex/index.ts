9d76fcd37692d8aca2af150fe8c46439b19138a9
jeongyou <wjddbwjd1205@naver.com>
2025-12-27 23:19:17 +0900

    feat: 레이아웃 구성을 위한 Flexbox 컴포넌트 추가

---
+++ src/shared/components/Flex/index.ts
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+
+type FlexProps = {
+  direction?: 'row' | 'column';
+  align?: 'stretch' | 'flex-start' | 'center' | 'flex-end';
+  justify?: 'flex-start' | 'center' | 'space-between' | 'space-around';
+  gap?: number;
+};
+
+const Flex = styled.div<FlexProps>`
+  display: flex;
+  flex-direction: ${({ direction = 'row' }) => direction};
+  align-items: ${({ align = 'stretch' }) => align};
+  justify-content: ${({ justify = 'flex-start' }) => justify};
+  gap: ${({ gap = 0 }) => `${gap}px`};
+`;
+
+export { Flex };
