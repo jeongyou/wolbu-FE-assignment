4376d01642cbdde566c53249d03f6993bcab0caf
jeongyou <wjddbwjd1205@naver.com>
2025-12-29 07:55:55 +0900

    refactor: 토스트 메세지가 상단에서 뜨도록 변경

--- src/shared/components/Toast/Toast.styles.ts
+++ src/shared/components/Toast/Toast.styles.ts
@@ -2,17 +2,18 @@ import styled from '@emotion/styled';
 
 export const Container = styled.div`
   position: fixed;
-  bottom: 24px;
+  top: 70px;
   left: 50%;
   transform: translateX(-50%);
   padding: 12px 16px;
   background: rgba(0, 0, 0, 0.8);
   border-radius: 8px;
   z-index: 1000;
-  font-size: 18px;
+  font-size: 16px;
   color: white;
 `;
 
 export const Message = styled.span`
   color: inherit;
+  white-space: pre-line;
 `;
