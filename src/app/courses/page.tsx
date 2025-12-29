393378d9cb0762b435419299521164ce66b8aac9
jeongyou <wjddbwjd1205@naver.com>
2025-12-29 19:06:34 +0900

    feat: RequireAuth를 위해 강의 목록 클라이언트 페이지 분리

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,21 +1,13 @@
-import Header from '@/shared/components/Header';
 import { getCourses } from '@/features/courses/api/getCourses';
-import CourseListContainer from '@/features/courses/components/CourseListContainer';
 import { GetCoursesParams } from '@/features/courses/api/types';
-import RequireAuth from '@/shared/guards/RequireAuth';
+import CoursesPageClient from '@/features/courses/CoursesPageClient';
 
 const LecturesPage = async () => {
   const initialParams: GetCoursesParams = { page: 0, size: 10, sort: 'recent' };
   const coursePage = await getCourses(initialParams);
 
   return (
-    <RequireAuth>
-      <Header title='강의 목록' />
-      <CourseListContainer
-        initialPage={coursePage}
-        initialParams={initialParams}
-      />
-    </RequireAuth>
+    <CoursesPageClient initialPage={coursePage} initialParams={initialParams} />
   );
 };
 
