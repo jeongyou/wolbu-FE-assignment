226bfd78cf94e8202a3b9757dbc4426439cfdb46
jeongyou <wjddbwjd1205@naver.com>
2025-12-29 16:28:17 +0900

    refactor: 검증 시 null(초기값)인 경우 추가

--- src/features/course-create/utils/validatePrice.ts
+++ src/features/course-create/utils/validatePrice.ts
@@ -1,5 +1,7 @@
-export const validatePrice = (value: number) => {
-  if (Number.isNaN(value)) return '숫자를 입력해주세요';
+export const validatePrice = (value: number | null) => {
+  if (value === null || Number.isNaN(value)) return '숫자를 입력해주세요';
+
   if (value <= 0) return '가격은 0보다 커야 합니다';
+
   return true;
 };
